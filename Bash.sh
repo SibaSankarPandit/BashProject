a600eb058c7a18dcbebae2d1b58b5e489ca635a4
sibasankar pandit <sibasankarpandit325@gmail.com>
2023-06-27 22:26:09 +0530

    Code not Working so i renamed bucket_name= abhishek-ultimate-bucket to 789-ultimate-bucket

--- Bash.sh
+++ Bash.sh
@@ -10,7 +10,7 @@ echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
+bucket_name="789-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
 email_address="sibasankar.pandit952@gmail.com"
